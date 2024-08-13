209e93a7bf96338819e8adb082258910c86a9606
Lala Shin <ppeanut@naver.com>
2024-08-13 14:13:50 +0900

    240813-2

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
+  const[searchKeyword, setSearchKeyword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    console.log(searchKeyword)
+    if(searchKeyword){
+      navigate(`/search/${searchKeyword}`);
+      setSearchKeyword('');
+    }
+  }
   return (
     <div id='search'>
       <div className='search__inner'>
@@ -8,12 +19,18 @@ const Search = () => {
                 <span className='ir'>검색</span>
             </label>
             <input 
-                type='text' 
+                type='search' 
                 name='searchInput' 
                 id='searchInput' 
                 autoComplete='off' 
                 className='search__input' 
                 placeholder='검색어를 입력해주세요!'
+                onChange={e=>setSearchKeyword(e.target.value)}
+                onKeyDown={e=>{
+                  if(e.key === 'Enter') {
+                    handleSearch();
+                  }
+                }}
             />
       </div>
     </div>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect,useState} from 'react'
 import Main from '../components/section/Main'
 
+import VideoSearch from '../components/videos/VideoSearch'
+import { useParams } from 'react-router-dom'
+
 const Search = () => {
+    const {searchID} = useParams();
+    const [videos, setVideos] = useState([]);
+
+    useEffect(()=>{
+        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=48&q=${searchID}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
+        )
+        .then(response => response.json())
+        .then(result =>{
+            console.log(result);
+            setVideos(result.items)
+        })
+        .catch(error => console.log(error));        
+    },[searchID]);
+
   return (
     <Main 
-    title='lala 유튜브채널' 
-    description='lala 유튜브채널에 오신것을 환영합니다.' >
-        Search
+    title='유튜브 검색' 
+    description='유튜브 검색 결과 페이지입니다.' >
+
+        <section id='searchPage'>
+            <div className='video__inner search'>
+                <VideoSearch videos={videos} />
+            </div>
+        </section>
+
     </Main>
   )
 }
